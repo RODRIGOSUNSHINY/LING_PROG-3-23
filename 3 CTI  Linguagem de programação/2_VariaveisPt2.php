6c0e38f4ba2e2304419a13fdc6ffe7c5d1c0aecb
RODRIGOSUNSHINY <125514493+RODRIGOSUNSHINY@users.noreply.github.com>
2023-02-22 22:11:39 -0200

    Add files via upload

---
+++ 3 CTI  Linguagem de programação/2_VariaveisPt2.php
@@ -0,0 +1,23 @@
+<?php
+$name = "Denicreidsson";
+$age = 24;
+echo $name;
+echo "<br>";
+echo $age;
+echo "<hr>";
+$b = true;
+echo gettype ($b);
+echo "<hr>";
+
+$nasc = 1994;
+echo gettype($nasc);
+
+echo "<hr>";
+$float = 99.90;
+echo gettype ($float);
+
+echo "<hr>";
+$float = 'Lala 123';
+echo gettype ($float);
+
+?>
