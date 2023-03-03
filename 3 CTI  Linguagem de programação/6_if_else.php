cf99fa79e5f19575b98716c5541e1b4634a5d7ae
RODRIGOSUNSHINY <125514493+RODRIGOSUNSHINY@users.noreply.github.com>
2023-03-03 21:51:03 -0200

    Add files via upload

--- 3 CTI  Linguagem de programação/6_if_else.php
+++ 3 CTI  Linguagem de programação/6_if_else.php
@@ -6,4 +6,21 @@ if ($idade < 18){
 } else {
 	echo 'Seja bem-vindo';
 }
+
+
+$idade = 12;
+
+if ($idade < 14){
+	echo 'Você vai ser do time infantil.';
+} else {
+	echo 'Você vai ser do time Junior.';
+}
+
+$nota = 60 ;
+
+if ($nota > 59){
+	echo 'APROVADO.';
+} else {
+	echo 'REPROVADO.';
+}
 ?>
